638db2c5146ff0efb09423b4da2e54ae9113301b
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-07-31 02:44:32 +0300

    Create EditAutoViewModel test file

---
+++ AutologgerTests/EditAuto/EditAutoViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  EditAutoViewModelTests.swift
+//  AutologgerTests
+//
+//  Created by Ramazan Ashurbekov on 31.07.2022.
+//
+
+import Combine
+import XCTest
+@testable import Autologger
+
+final class EditAutoViewModelTests: XCTestCase {
+    private var editAutoViewModel: EditAutoViewModel!
+
+    func testFetchMakes() {
+
+    }
+
+    func testSave() {
+
+    }
+}
