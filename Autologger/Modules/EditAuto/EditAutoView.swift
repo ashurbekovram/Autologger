7a3e28d54dc7d80d12d452c1cb6af85471593f28
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-08-02 00:25:37 +0300

    Change form to VStack

--- Autologger/Modules/EditAuto/EditAutoView.swift
+++ Autologger/Modules/EditAuto/EditAutoView.swift
@@ -14,12 +14,15 @@ import NetworkManager
 struct EditAutoView<ViewModel: EditAutoViewModelProtocol>: View {
     @ObservedObject var viewModel: ViewModel
 
+    @State private var showMakePicker: Bool = false
+    @State private var showYearPicker: Bool = false
+
     var body: some View {
-        VStack {
+        ScrollView {
             if viewModel.isLoading {
                 ProgressView()
             } else {
-                Form {
+                VStack {
                     if let error = viewModel.error {
                         Text(error.localizedDescription)
                             .foregroundColor(.red)
@@ -28,32 +31,57 @@ struct EditAutoView<ViewModel: EditAutoViewModelProtocol>: View {
                             viewModel.fetchMakes()
                         }
                     } else {
-                        Section {
+                        HStack {
+                            Button("Select make") {
+                                withAnimation {
+                                    showMakePicker.toggle()
+                                }
+                            }
+                            Spacer()
+                            Text(viewModel.selectedMake?.name ?? "None")
+                        }
+                        if showMakePicker {
                             Picker("Select make", selection: $viewModel.selectedMake) {
                                 Text("No make").tag(nil as Make?)
                                 ForEach(viewModel.makes) { make in
                                     Text(make.name).tag(make as Make?)
                                 }
                             }
-                            .pickerStyle(.menu)
+                            .pickerStyle(.wheel)
+                        }
+
+                        HStack {
+                            Button("Select year") {
+                                withAnimation {
+                                    showYearPicker.toggle()
+                                }
+                            }
+                            Spacer()
+                            Text(viewModel.selectedYear.description)
+                        }
+                        if showYearPicker {
                             Picker("Select year", selection: $viewModel.selectedYear) {
                                 ForEach(viewModel.years, id: \.self) { year in
                                     Text(year.description)
                                 }
                             }
                             .pickerStyle(.wheel)
-                            TextField("Enter Model", text: $viewModel.model)
-                            TextField("Enter VIN", text: $viewModel.vin)
                         }
-                        Section {
-                            Button("Save") {
-                                viewModel.save()
-                            }
+                        TextField("Enter Model", text: $viewModel.model)
+                        TextField("Enter VIN", text: $viewModel.vin)
+                        Button("Save") {
+                            viewModel.save()
                         }
+                        Spacer()
                     }
                 }
+                .padding()
+                .background(Color("BackgroundSecondary"))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
             }
         }
+        .padding()
+        .background(Color("BackgroundPrimary"))
         .navigationTitle("Edit auto")
         .onAppear {
             viewModel.fetchMakes()
@@ -69,7 +97,7 @@ final class EditAutoViewModelMock: EditAutoViewModelProtocol {
     @Published var isLoading: Bool = false
     @Published var error: Error? = nil
     @Published var makes: [Make] = []
-    @Published var years: [Int] = [2005, 2006, 2007, 2008]
+    @Published var years: [Int] = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013]
 
     @Published var selectedMake: Make? = nil
     @Published var selectedYear: Int = 2005
