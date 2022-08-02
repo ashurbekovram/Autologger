2cdf407bd3f8c20417dd872baa3658f8ca8f9ce4
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-08-02 13:31:15 +0300

    Change form to VStack

--- Autologger/Modules/EditAuto/EditAutoView.swift
+++ Autologger/Modules/EditAuto/EditAutoView.swift
@@ -23,66 +23,91 @@ struct EditAutoView<ViewModel: EditAutoViewModelProtocol>: View {
                 ProgressView()
             } else {
                 VStack {
-                    if let error = viewModel.error {
-                        Text(error.localizedDescription)
-                            .foregroundColor(.red)
-                            .font(.system(size: 16, weight: .semibold, design: .default))
-                        Button("Try again") {
-                            viewModel.fetchMakes()
-                        }
-                    } else {
-                        HStack {
-                            Button("Select make") {
-                                withAnimation {
-                                    showMakePicker.toggle()
-                                }
+                    VStack {
+                        if let error = viewModel.error {
+                            Text(error.localizedDescription)
+                                .foregroundColor(.red)
+                                .font(.system(size: 16, weight: .semibold, design: .default))
+                            Button("Try again") {
+                                viewModel.fetchMakes()
                             }
-                            Spacer()
-                            Text(viewModel.selectedMake?.name ?? "None")
-                        }
-                        if showMakePicker {
-                            Picker("Select make", selection: $viewModel.selectedMake) {
-                                Text("No make").tag(nil as Make?)
-                                ForEach(viewModel.makes) { make in
-                                    Text(make.name).tag(make as Make?)
+                        } else {
+                            Group {
+                                Button {
+                                    withAnimation {
+                                        showMakePicker.toggle()
+                                    }
+                                } label: {
+                                    HStack {
+                                        Text("Make")
+                                            .foregroundColor(Color("TextPrimary"))
+                                        Spacer()
+                                        Text(viewModel.selectedMake?.name ?? "None")
+                                    }
                                 }
-                            }
-                            .pickerStyle(.wheel)
-                        }
-
-                        HStack {
-                            Button("Select year") {
-                                withAnimation {
-                                    showYearPicker.toggle()
+                                if showMakePicker {
+                                    Picker("Select make", selection: $viewModel.selectedMake) {
+                                        Text("No make").tag(nil as Make?)
+                                        ForEach(viewModel.makes) { make in
+                                            Text(make.name).tag(make as Make?)
+                                        }
+                                    }
+                                    .pickerStyle(.wheel)
                                 }
+                                Divider()
                             }
-                            Spacer()
-                            Text(viewModel.selectedYear.description)
-                        }
-                        if showYearPicker {
-                            Picker("Select year", selection: $viewModel.selectedYear) {
-                                ForEach(viewModel.years, id: \.self) { year in
-                                    Text(year.description)
+                            Group {
+                                Button {
+                                    withAnimation {
+                                        showYearPicker.toggle()
+                                    }
+                                } label: {
+                                    HStack {
+                                        Text("Year")
+                                            .foregroundColor(Color("TextPrimary"))
+                                        Spacer()
+                                        Text(viewModel.selectedYear.description)
+                                    }
                                 }
+                                if showYearPicker {
+                                    Picker("Select year", selection: $viewModel.selectedYear) {
+                                        ForEach(viewModel.years, id: \.self) { year in
+                                            Text(year.description)
+                                        }
+                                    }
+                                    .pickerStyle(.wheel)
+                                }
+                                Divider()
                             }
-                            .pickerStyle(.wheel)
+                            Group {
+                                TextField("Enter Model", text: $viewModel.model)
+                                Divider()
+                                TextField("Enter VIN", text: $viewModel.vin)
+                            }
                         }
-                        TextField("Enter Model", text: $viewModel.model)
-                        TextField("Enter VIN", text: $viewModel.vin)
-                        Button("Save") {
-                            viewModel.save()
-                        }
-                        Spacer()
                     }
+                    .padding()
+                    .background(Color("BackgroundSecondary"))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    Spacer(minLength: 32)
+                    Button {
+                        viewModel.save()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("Save").frame(height: 40, alignment: .center)
+                            Spacer()
+                        }
+                    }
+                    .background(Color("BackgroundSecondary"))
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
                 }
                 .padding()
-                .background(Color("BackgroundSecondary"))
-                .clipShape(RoundedRectangle(cornerRadius: 8))
             }
         }
-        .padding()
         .background(Color("BackgroundPrimary"))
         .navigationTitle("Edit auto")
+        .navigationBarTitleDisplayMode(.large)
         .onAppear {
             viewModel.fetchMakes()
         }
