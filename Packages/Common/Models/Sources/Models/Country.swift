ec65cb64218592106123855751dbee0829314e73
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-12-10 17:25:54 +0300

    Models update

---
+++ Packages/Common/Models/Sources/Models/Country.swift
@@ -0,0 +1,11 @@
+//
+//  Country.swift
+//  
+//
+//  Created by Ramazan Ashurbekov on 10.12.2022.
+//
+
+public struct Country: Decodable, Hashable {
+    let id: Int
+    let name: String
+}
--- Packages/Common/Models/Sources/Models/Make.swift
+++ Packages/Common/Models/Sources/Models/Make.swift
@@ -8,12 +8,49 @@
 public struct Make: Decodable, Hashable, Identifiable {
     public let id: Int
     public let name: String
+    public let description: String
+    public let foundingDate: String
+    public let country: Country
+    public let series: [Series]
+
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case description
+        case foundingDate
+        case country
+        case series
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.description = try container.decode(String.self, forKey: .description)
+        self.foundingDate = try container.decode(String.self, forKey: .foundingDate)
+        self.country = try container.decode(Country.self, forKey: .country)
+        self.series = try container.decode([Series].self, forKey: .series)
+    }
 
     public init(
         id: Int,
-        name: String
+        name: String,
+        description: String,
+        foundingDate: String,
+        country: Country,
+        series: [Series]
     ) {
         self.id = id
         self.name = name
+        self.description = description
+        self.foundingDate = foundingDate
+        self.country = country
+        self.series = series
     }
+}
+
+
+public struct Series: Decodable, Hashable {
+    let id: Int
+    let name: String
 }
